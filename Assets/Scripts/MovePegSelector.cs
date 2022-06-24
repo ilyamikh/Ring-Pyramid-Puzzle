b8f331367c7ee4ccd12883eade9d2e4c4768d19e
ilyamikh <36613951+ilyamikh@users.noreply.github.com>
2022-06-24 19:10:36 -0400

    Ghost Ring Functionality
    Changed the selection indicator color to yellow in code.

--- Assets/Scripts/MovePegSelector.cs
+++ Assets/Scripts/MovePegSelector.cs
@@ -7,7 +7,7 @@ public class MovePegSelector : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameObject.GetComponentInChildren<Renderer>().material.color = new Color(1.0f, 0.92f, 0.016f);
     }
 
     // Update is called once per frame
